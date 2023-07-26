8a10097a7d02bbabe937788b6eaaea2c8f206c59
Lip3xvx <110494737+Lip3xvx@users.noreply.github.com>
2023-07-26 13:27:45 -0500

    Add files via upload

---
+++ tiendaMusic/js/products.js
@@ -0,0 +1,41 @@
+const productos = [
+  {
+    id:1,
+    nombre: "The Chronic",
+    precio: 30000,
+    artista: "Dr. Dre",
+    año: 1992,
+    img: "https://caracoltv.brightspotcdn.com/dims4/default/53931b4/2147483647/strip/true/crop/828x820+0+0/resize/828x820!/quality/90/?url=http%3A%2F%2Fcaracol-brightspot.s3.amazonaws.com%2F66%2Fbb%2Fc83e82a741cfbd15451f50d2a443%2Fdr-dre-the-chronic.jpg",
+    cantidad: 1
+  },
+
+  {
+    id:2,
+    nombre: "Ready to Die",
+    precio: 80000,
+    artista: "The Notorious B.I.G.",
+    año: 1994,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Ready_To_Die.jpg/220px-Ready_To_Die.jpg",
+    cantidad: 1
+  },
+
+  {
+    id:3,
+    nombre: "Swimming",
+    precio: 35000,
+    artista: "Mac Miller",
+    año: 2018,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Mac_Miller_-_Swimming.png/220px-Mac_Miller_-_Swimming.png",
+    cantidad: 1
+  },
+
+  {
+    id:4,
+    nombre: "Donuts",
+    precio: 50000,
+    artista: "J. Dilla",
+    año: 2006,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/1/14/Dilladonutscover.jpg/220px-Dilladonutscover.jpg",
+    cantidad: 1
+  },
+]
